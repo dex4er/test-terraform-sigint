497b4c1bd18a04728b511d6e6ce9800a0a8e879f
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-03-19 22:55:40 +0100

    Clean up

--- main.tf
+++ main.tf
@@ -14,17 +14,12 @@ variable "duration" {
   default = "1m"
 }
 
+resource "time_static" "this" {}
+
 resource "time_sleep" "duration" {
   create_duration  = var.duration
   destroy_duration = var.duration
-}
-
-resource "time_static" "static" {}
-
-resource "time_sleep" "duration2" {
-  create_duration  = var.duration
-  destroy_duration = var.duration
   depends_on = [
-    time_static.static
+    time_static.this,
   ]
 }
