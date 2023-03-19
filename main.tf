1af258154de55a97e5606c7d9622e01cdbe5590c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-03-19 19:35:03 +0100

    time_sleep.duration2

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "time_sleep" "duration" {
   create_duration  = var.duration
   destroy_duration = var.duration
 }
+
+resource "time_static" "static" {}
+
+resource "time_sleep" "duration2" {
+  create_duration  = var.duration
+  destroy_duration = var.duration
+  depends_on = [
+    time_static.static
+  ]
+}
